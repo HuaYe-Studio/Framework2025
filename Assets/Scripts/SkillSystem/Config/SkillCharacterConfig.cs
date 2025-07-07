64400a16d19d4c489d69d22b6bea994b0bab1e73
原初z <3392432441@qq.com>
2025-07-07 23:37:17 +0800

    feat : 用Odin初步搭建技能编辑器面板中的角色模型部分

---
+++ Assets/Scripts/SkillSystem/Config/SkillCharacterConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace SkillSystem.Config
+{
+    [Serializable]
+    [HideMonoScript]
+    public class SkillCharacterConfig
+    {
+        [AssetList]
+        [LabelText("角色模型")]
+        [PreviewField(70, ObjectFieldAlignment.Center)]
+        public GameObject SkillCharacter;
+        
+        [TitleGroup("技能渲染" , "所有技能渲染数据会在技能释放时触发")]
+        [LabelText("技能动画")]
+        public AnimationClip AnimationClip;
+
+    
+        [BoxGroup("动画数据")]
+        [ProgressBar(0,100,r:0,g:255,b:0,Height = 30)]
+        [HideLabel]
+        public short animProgess = 0;
+    }
+}
---
+++ Assets/Scripts/SkillSystem/EditorWindow/SkillComplierWindow.cs
@@ -0,0 +1,21 @@
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using SkillSystem.Config;
+using UnityEditor;
+using UnityEngine;
+
+namespace SkillSystem.EditorWindow
+{
+    public class SkillComplierWindow : OdinEditorWindow
+    {
+        [MenuItem("Skill/技能编译器")]
+        public static SkillComplierWindow OpenWindow()
+        {
+            return GetWindowWithRect<SkillComplierWindow>(new Rect(0,0,1000,600));
+        }
+        
+        [TabGroup("Skill" , "模型动画数据" , SdfIconType.PersonFill , TextColor = "orange")]
+        public SkillCharacterConfig CharacterConfig = new SkillCharacterConfig();
+        
+    }
+}
